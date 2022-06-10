ad79b68210d8b9cf5a7234daf7e87cd75f264fe0
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-11 01:28:42 +0400

    fin

--- assets/js/dashboard/dashboardHeader.js
+++ assets/js/dashboard/dashboardHeader.js
@@ -1,6 +1,16 @@
 const profileBTN = document.querySelector('.profileBTN')
+const barsBTN = document.querySelector('.barsBTN')
+const notificationsBTN = document.querySelector('.notificationsBTN')
 const dropdownProfile = document.querySelector('.dropdownProfile')
+const dropdownBars = document.querySelector('.dropdownBars')
+const dropdownNotifications = document.querySelector('.dropdownNotifications')
 
 profileBTN.addEventListener("click",(e) => {
     dropdownProfile.classList.toggle('hidden')
+})
+barsBTN.addEventListener("click",(e) => {
+    dropdownBars.classList.toggle('hidden')
+})
+notificationsBTN.addEventListener("click",(e) => {
+    dropdownNotifications.classList.toggle('hidden')
 })
