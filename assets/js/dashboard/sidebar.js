3892fa51ea33e27d69fb589312b18a78901ca98a
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-12 19:05:46 +0400

    ff

--- assets/js/dashboard/sidebar.js
+++ assets/js/dashboard/sidebar.js
@@ -8,12 +8,21 @@ const smallLogo = document.querySelector('.smallLogo')
 const balanceMainDiv = document.querySelector('.balanceMainDiv')
 const sidebarListTexts = document.querySelectorAll('.sidebarList ul li a .text')
 const showMoreContentTexts = document.querySelectorAll('.showMoreContent ul li a .text')
-
 showMoreBTN.addEventListener('click' , (e) => {
     showMoreBTN.classList.toggle('active')
     showMoreContent.classList.toggle('hidden')
 })
 closeOpenSidebarButton.addEventListener('click' , (e) => {
+    if(localStorage.getItem('sidebar') == "smallSidebar"){
+        localStorage.setItem('sidebar',"bigSidebar")
+
+    }
+    else if(localStorage.getItem('sidebar') == "bigSidebar"){
+        localStorage.setItem('sidebar',"smallSidebar")
+
+
+    }
+
     dashboardContent.classList.toggle('bigDashboard')
     sidebar.classList.toggle('smallSidebar')
     bigLogo.classList.toggle('notShowing')
@@ -48,6 +57,21 @@ window.addEventListener("resize" , (e) => {
       }
 })
 window.addEventListener("DOMContentLoaded" , (e) => {
+    if(localStorage.getItem('sidebar') == "smallSidebar"){
+        dashboardContent.classList.toggle('bigDashboard')
+    sidebar.classList.toggle('smallSidebar')
+    bigLogo.classList.toggle('notShowing')
+    smallLogo.classList.toggle('notShowing')
+    balanceMainDiv.classList.toggle('notShowing')
+    for(let i= 0 ; i < sidebarListTexts.length;i++){
+        sidebarListTexts[i].classList.toggle('notShowing')
+    }
+    showMoreBTN.classList.toggle('notShowing')
+    showMoreContent.classList.remove('hidden')
+    for(let i= 0 ; i < showMoreContentTexts.length;i++){
+        showMoreContentTexts[i].classList.toggle('notShowing')
+    }
+    }
     if(window.outerWidth <= 768){
         dashboardContent.classList.toggle('bigDashboard')
         sidebar.classList.toggle('smallSidebar')
