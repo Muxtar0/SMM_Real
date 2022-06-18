72593dd085074ffa1317c367d778b5065a01c1b6
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-18 17:38:40 +0400

    f

--- assets/js/dashboard/filterDropDown.js
+++ assets/js/dashboard/filterDropDown.js
@@ -2,16 +2,16 @@ const filterBtnFirst = document.querySelector('.filterBtnFirst');
 const filterItemFirst = document.querySelector('.filterItemFirst');
 const filterDropDownDivFirst = document.querySelector('.filterDropDownDivFirst');
 
-// filterBtnFirst.addEventListener('click' , (e) => {
-//     filterDropDownDivFirst.classList.toggle('hidden')
-// })
-
-window.addEventListener('click' , (e) => {
-    if(e.target.className == "filterBtnFirst"){
-        filterDropDownDivFirst.classList.toggle('hidden')
-    }
-    else if(e.target.className != "filterItemFirst"){
-        filterDropDownDivFirst.classList.toggle('hidden')
-
-    }
+filterBtnFirst.addEventListener('click' , (e) => {
+    filterDropDownDivFirst.classList.toggle('hidden')
 })
+
+// window.addEventListener('click' , (e) => {
+//     if(e.target.className == "filterBtnFirst"){
+//         filterDropDownDivFirst.classList.toggle('hidden')
+//     }
+//     else if(e.target.className != "filterItemFirst"){
+//         filterDropDownDivFirst.classList.toggle('hidden')
+
+//     }
+// })
