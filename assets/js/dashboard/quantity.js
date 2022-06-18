ac6bfe151ec5bc37b4046656cb4b9ec5afac0dd4
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-18 15:40:10 +0400

    ff

--- assets/js/dashboard/quantity.js
+++ assets/js/dashboard/quantity.js
@@ -3,6 +3,10 @@ const uploadMoneyBTNs = document.querySelectorAll('.uploadMoneyBTN')
 
 for(let i = 0 ; i < uploadMoneyBTNs.length;i++){
     uploadMoneyBTNs[i].addEventListener('click' , (e) => {
+        for(let l = 0 ; l < uploadMoneyBTNs.length;l++){
+            uploadMoneyBTNs[l].classList.remove('active')
+        }
+        uploadMoneyBTNs[i].classList.add('active')
         quantityInput.value = uploadMoneyBTNs[i].querySelector('.text h4').textContent
     })
 }
