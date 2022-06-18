a35d59e5009a557dcc4223014bfb1df9d3927d74
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-18 23:55:35 +0400

    finish

--- assets/js/dashboard/dashboardHeader.js
+++ assets/js/dashboard/dashboardHeader.js
@@ -6,6 +6,44 @@ const dropdownBars = document.querySelector('.dropdownBars')
 const dropdownNotifications = document.querySelector('.dropdownNotifications')
 
 
+const themeChangeBtn = document.querySelector('.themeChangeBtn')
+const body = document.querySelector('body')
+let theme = "light";
+
+
+const controlTheme = () => {
+    if(localStorage.getItem('theme') == null){
+        // Do nothing
+    }
+    else if(localStorage.getItem('theme') == "dark"){
+        body.classList.add('dark')
+        themeChangeBtn.innerHTML='<i class="fa-solid fa-sun"></i>'
+        theme = "dark";
+        
+    }
+    else if(localStorage.getItem('theme') == "light"){
+        body.classList.remove('dark')
+        themeChangeBtn.innerHTML='<i class="fa-solid fa-moon"></i>'
+        theme = "light"
+    }
+}
+controlTheme();
+
+themeChangeBtn.addEventListener('click' , (e) => {
+    if(theme == "light"){
+        body.classList.add('dark')
+        themeChangeBtn.innerHTML='<i class="fa-solid fa-sun"></i>'
+        theme = "dark";
+        localStorage.setItem('theme','dark')
+    }
+    else if (theme == "dark"){
+        body.classList.remove('dark')
+        themeChangeBtn.innerHTML='<i class="fa-solid fa-moon"></i>'
+        theme = "light"
+        localStorage.setItem('theme','light')
+    }
+}) 
+
 const dashboardHeader = document.querySelector('.dashboardHeader')
 
 profileBTN.addEventListener("click",(e) => {
