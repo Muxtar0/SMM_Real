4113df775dccff9091c723c64e0fe9a682d7ae93
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-26 23:40:10 +0400

    ff

---
+++ assets/js/dashboard/requestBottomAccordion.js
@@ -0,0 +1,9 @@
+const bottomAccordions = document.querySelectorAll('.bottomAccordion')
+const bottomAccordionsButtons = document.querySelectorAll('.bottomAccordion button')
+
+
+for(let i = 0 ; i < bottomAccordionsButtons.length;i++){
+    bottomAccordionsButtons[i].addEventListener('click' , (e) => {
+        bottomAccordions[i].classList.toggle('active')
+    })
+}
