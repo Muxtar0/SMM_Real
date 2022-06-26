e4b8dd428c5671c8405b8f80148a4e8b55515566
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-26 17:12:31 +0400

    gg

--- assets/js/dashboard/supportRequest.js
+++ assets/js/dashboard/supportRequest.js
@@ -1,16 +1,22 @@
 const supportTabContent = document.querySelector('.supportRequestContents .supportRequestTabContent .content')
+const createSupportRequestContentLeft = document.querySelector('.supportRequestMainSection .leftContentCol .createSupportRequestContent')
 const createSupportFirstBtn = document.querySelector('.createSupportFirstBtn')
 const createSupportBtnSecond = document.querySelector('.createSupportBtnSecond')
 const leftContentCol = document.querySelector('.supportRequestMainSection .leftContentCol')
 const rightContentCol = document.querySelector('.supportRequestMainSection .rightContentCol')
+const bgCloseRequest = document.querySelector('.supportRequestMainSection .rightContentCol .bg-close-request')
+const closeSupportModalBTN = document.querySelector('.supportRequestMainSection .rightContentCol .closeSupportModalBTN')
 
 createSupportFirstBtn.addEventListener('click' , (e) => {
     supportTabContent.classList.add('active')
+    createSupportRequestContentLeft.classList.add('hidden')
 })
 createSupportBtnSecond.addEventListener('click' , (e) => {
     rightContentCol.classList.add('active')
-    if(window.innerWidth > 992) {
-        leftContentCol.classList.remove('col-lg-12')
-        leftContentCol.classList.add('col-lg-8')
-    }
+})
+bgCloseRequest.addEventListener('click' , (e) => {
+    rightContentCol.classList.remove('active')
+})
+closeSupportModalBTN.addEventListener('click' , (e) => {
+    rightContentCol.classList.remove('active')
 })
--- assets/js/index3/questions.js
+++ assets/js/index3/questions.js
@@ -6,5 +6,9 @@ for(let i = 0 ; i < questionBTNs.length; i++){
     questionBTNs[i].addEventListener('click' , (e) => {
         
         questionInfos[i].classList.toggle('active')
+        setTimeout(() => {
+        questionInfos[i].classList.toggle('active2')
+            
+        }, 300);
     }) 
 }
