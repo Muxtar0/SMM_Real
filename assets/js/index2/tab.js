8bb163e44b3a21902ae11291acf11f64718c97e3
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-30 01:36:12 +0400

    ff

--- assets/js/index2/tab.js
+++ assets/js/index2/tab.js
@@ -7,12 +7,15 @@ for(let i = 0 ; i < tabBtns.length;i++){
             tabContents[i].classList.remove('active')
             tabContents[i].classList.remove('activ2')
         }
-        // for(lei l = 0 ;)
+        for(let i = 0 ; i < tabBtns.length;i++){
+            tabBtns[i].classList.remove('active')
+        }
         if(tabBtns[i].getAttribute('data-target') == tabContents[i].getAttribute('data-way')){
             tabContents[i].classList.add('active');
             setTimeout(() => {
                 tabContents[i].classList.add('active2');
             }, 100);
+            tabBtns[i].classList.add('active')
         }
     })
 }
