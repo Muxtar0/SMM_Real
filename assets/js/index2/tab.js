6d23a450a401cb0c33e8b5da637d761c5d678933
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-06-30 01:32:25 +0400

    FF

---
+++ assets/js/index2/tab.js
@@ -0,0 +1,18 @@
+const tabContents = document.querySelectorAll('#ProfessionallyExperienced .tabContent') 
+const tabBtns = document.querySelectorAll('#ProfessionallyExperienced .tabBtn') 
+
+for(let i = 0 ; i < tabBtns.length;i++){
+    tabBtns[i].addEventListener('click' , (e) => {
+        for(let i = 0 ; i < tabContents.length;i++){
+            tabContents[i].classList.remove('active')
+            tabContents[i].classList.remove('activ2')
+        }
+        // for(lei l = 0 ;)
+        if(tabBtns[i].getAttribute('data-target') == tabContents[i].getAttribute('data-way')){
+            tabContents[i].classList.add('active');
+            setTimeout(() => {
+                tabContents[i].classList.add('active2');
+            }, 100);
+        }
+    })
+}
