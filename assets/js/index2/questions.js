1e087b68e49d64f132d96b62427036e946c2dd3d
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-07-01 11:56:08 +0400

    ff

--- assets/js/index2/questions.js
+++ assets/js/index2/questions.js
@@ -1,12 +1,8 @@
 const questionBTNs = document.querySelectorAll('.questionBTN')
 const questionItems = document.querySelectorAll('.questionItem')
-
-for(let i = 0 ; i , questionBTNs.length;i++){
+for(let i = 0 ; i < questionBTNs.length;i++){
     questionBTNs[i].addEventListener('click' , (e) => {
         questionItems[i].classList.toggle('active')
-        setTimeout(() => {
-        questionItems[i].classList.toggle('active2')
-            
-        }, 100);
+        
     })
 }
