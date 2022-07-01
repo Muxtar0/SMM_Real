31b53e38b5c781e25006266d23d8d5d2566c7965
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-07-01 19:15:46 +0400

    ss

--- assets/js/dashboard/agreementTab.js
+++ assets/js/dashboard/agreementTab.js
@@ -3,23 +3,29 @@ const tabContent = document.querySelectorAll('.tabContent')
 
 for(let i= 0 ; i < tabBtn.length;i++){
     tabBtn[i].addEventListener('click' , (e) => {
-        let btnWay = tabBtn[i].getAttribute('data-way');
-        for(let i= 0 ; i < tabBtn.length;i++){
-            tabBtn[i].classList.remove('active')
+        if(tabBtn[i].classList.contains('active')){
+
         }
-        for(let i= 0 ; i < tabContent.length;i++){
-            tabContent[i].classList.remove('active')
-            tabContent[i].classList.remove('active2')
-        }
-        for(let i= 0 ; i < tabContent.length;i++){
-            if(btnWay == tabContent[i].getAttribute('data-kind')){
-                tabContent[i].classList.add("active")
-                setTimeout(() => {
-                tabContent[i].classList.add("active2")
-                    
-                }, 100);
+        else{
+            let btnWay = tabBtn[i].getAttribute('data-way');
+            for(let i= 0 ; i < tabBtn.length;i++){
+                tabBtn[i].classList.remove('active')
             }
+            for(let i= 0 ; i < tabContent.length;i++){
+                tabContent[i].classList.remove('active')
+                tabContent[i].classList.remove('active2')
+            }
+            for(let i= 0 ; i < tabContent.length;i++){
+                if(btnWay == tabContent[i].getAttribute('data-kind')){
+                    tabContent[i].classList.add("active")
+                    setTimeout(() => {
+                    tabContent[i].classList.add("active2")
+                        
+                    }, 100);
+                }
+            }
+            tabBtn[i].classList.add("active")
         }
-        tabBtn[i].classList.add("active")
+        
     })
 }
