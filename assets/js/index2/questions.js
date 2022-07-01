fbb52d2a87cfce19470a638dc8c3c7847338a657
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-07-02 01:58:03 +0400

    ff

--- assets/js/index2/questions.js
+++ assets/js/index2/questions.js
@@ -2,6 +2,12 @@ const questionBTNs = document.querySelectorAll('.questionBTN')
 const questionItems = document.querySelectorAll('.questionItem')
 for(let i = 0 ; i < questionBTNs.length;i++){
     questionBTNs[i].addEventListener('click' , (e) => {
+        for(let i = 0 ; i < questionBTNs.length;i++){
+            if(questionBTNs[i].classList.contains('active')){
+                questionItems[i].classList.remove('active')
+            questionBTNs[i].classList.remove('active')
+            }
+        }
         questionItems[i].classList.toggle('active')
         questionBTNs[i].classList.toggle('active')
     })
