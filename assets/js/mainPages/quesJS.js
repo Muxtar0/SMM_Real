96d95b6bcc6eb8b8bc927820ffd522c3aa451e07
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-07-02 13:37:25 +0400

    ff

--- assets/js/mainPages/quesJS.js
+++ assets/js/mainPages/quesJS.js
@@ -1,16 +1,29 @@
 const accordionHeader = document.querySelectorAll('.accordion-header') 
 const accordionHeaderBtns = document.querySelectorAll('.accordion-header button') 
 const accordionCollapses = document.querySelectorAll('.accordion-collapse') 
-for(let i =0;i<accordionHeader.length;i++){
-    accordionHeader[i].addEventListener('click',(e) =>  {
-        for(let i =0;i<accordionHeaderBtns.length;i++){
-            accordionHeaderBtns[i].classList.add('collapsed')
-            accordionHeaderBtns[i].setAttribute('aria-expanded',false);
 
-        }
-        for(let i =0;i<accordionCollapses.length;i++){
-            accordionCollapses[i].classList.remove('show')
-        }
-    })
+const leftAcc = document.querySelector('.leftAcc')
 
-}
+
+leftAcc.addEventListener('click' ,(e) => {
+    e.stopPropagation();
+})
+
+window.addEventListener('click' , (e) => {
+    if(!e.target.matches(".leftAcc")){
+
+        for(let i =0;i<accordionHeader.length;i++){
+            accordionHeader[i].addEventListener('click',(e) =>  {
+                for(let i =0;i<accordionHeaderBtns.length;i++){
+                    accordionHeaderBtns[i].classList.add('collapsed')
+                    accordionHeaderBtns[i].setAttribute('aria-expanded',false);
+        
+                }
+                for(let i =0;i<accordionCollapses.length;i++){
+                    accordionCollapses[i].classList.remove('show')
+                }
+            })
+        
+        }
+    }
+})
