a11f15395143981b44f1b737dd7020b3d2ca3da8
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-07-14 23:53:58 +0400

    ff

--- assets/js/mainPages/quesJS.js
+++ assets/js/mainPages/quesJS.js
@@ -1,19 +1,26 @@
 const accordionHeader = document.querySelectorAll('.accordion-headerrightAcc') 
 const accordionHeaderBtns = document.querySelectorAll('.accordion-headerrightAcc button') 
 const accordionCollapses = document.querySelectorAll('.accordion-collapserightAcc') 
-
+let activeBtn;
 for(let i =0;i<accordionHeader.length;i++){
     accordionHeader[i].addEventListener('click',(e) =>  {
-        for(let i =0;i<accordionHeaderBtns.length;i++){
-            accordionHeaderBtns[i].classList.add('collapsed')
-            accordionHeaderBtns[i].classList.remove('active')
-            accordionHeaderBtns[i].setAttribute('aria-expanded',false);
+        
+        for(let l =0;l<accordionHeaderBtns.length;l++){
+            accordionHeaderBtns[l].classList.add('collapsed')
+            if(accordionHeader[i].querySelector('button') != accordionHeaderBtns[l]){
+                accordionHeaderBtns[l].classList.remove('active')
+            }
+            accordionHeaderBtns[l].setAttribute('aria-expanded',false);
 
         }
-        accordionHeader[i].querySelector('button').classList.add('active')
+        accordionHeader[i].querySelector('button').classList.toggle('active')
+
+        
         for(let i =0;i<accordionCollapses.length;i++){
             accordionCollapses[i].classList.remove('show')
         }
+        activeBtn = accordionHeader[i].querySelector('button');
+
     })
 
 }
@@ -26,13 +33,17 @@ const accordionHeaderBtnsLeft = document.querySelectorAll('.accordion-headerleft
 const accordionCollapsesLeft = document.querySelectorAll('.accordion-collapseleftAcc') 
 for(let i =0;i<accordionHeaderLeft.length;i++){
     accordionHeaderLeft[i].addEventListener('click',(e) =>  {
-        for(let i =0;i<accordionHeaderBtnsLeft.length;i++){
-            accordionHeaderBtnsLeft[i].classList.add('collapsed')
-            accordionHeaderBtnsLeft[i].classList.remove('active')
+        for(let l =0;l<accordionHeaderBtnsLeft.length;l++){
+            accordionHeaderBtnsLeft[l].classList.add('collapsed')
 
-            accordionHeaderBtnsLeft[i].setAttribute('aria-expanded',false);
+            if(accordionHeaderLeft[i].querySelector('button') != accordionHeaderBtnsLeft[l]){
+                accordionHeaderBtnsLeft[l].classList.remove('active')
+
+            }
+
+            accordionHeaderBtnsLeft[l].setAttribute('aria-expanded',false);
         }
-        accordionHeaderLeft[i].querySelector('button').classList.add('active')
+        accordionHeaderLeft[i].querySelector('button').classList.toggle('active')
 
         for(let i =0;i<accordionCollapsesLeft.length;i++){
             accordionCollapsesLeft[i].classList.remove('show')
